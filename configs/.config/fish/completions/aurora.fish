a0c47ff8740e6ef9ea349b05632715a7f1766734
rvdxse <kirillfilch@gmail.com>
2026-05-07 23:12:58 +0300

    welcome script for waybar and completions

---
+++ configs/.config/fish/completions/aurora.fish
@@ -0,0 +1,4 @@
+complete -c aurora -f -a pacman-search -d "Search pacman packages"
+
+complete -c aurora -f -a pacman-remove -d "Delete pacman packages"
+complete -c aurora -f -a system-update -d "Update all system"
---
+++ configs/.config/hypr/scripts/welcome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+python ~/.local/bin/wallpaper_change.py ~/Pictures/Wallpapers/shaded_landscape.jpg
+pkill waybar
+waybar &
+notify-send "Aurora is ready ✨" \
+  "Your system has been configured successfully.
+
+Press Super + Space to launch apps.
+Press Super + Return to open a terminal.
+
+Enjoy your setup."
+
+CONFIG="$HOME/.config/hypr/hyprland.conf"
+
+if [ -f "$CONFIG" ]; then
+  sed -i "/exec-once = .*welcome.sh/s/^/# /" "$CONFIG"
+fi
